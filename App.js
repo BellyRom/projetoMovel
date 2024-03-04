82390ff2d302196450cea1683b8bf89e949da82b
Isabelly vitória romão da silva <ivrs@aluno.ifal.edu.br>
2024-03-04 15:21:28 -0300

    Adição de if ternário para ano/anos

--- App.js
+++ App.js
@@ -5,8 +5,21 @@ const getNomeCompleto = (nome , sobrenome) => {
   return nome + ' ' + sobrenome;
 }
 
-const Cat = ({nome, sobrenome, idade}) => {2
-  return <Text>Hello, I am {getNomeCompleto(nome, sobrenome).toUpperCase()} and my age is {idade}!</Text>
+const Cat = ({nome, sobrenome, idade}) => {
+//   let texto = ' anos'
+//   if(idade < 1){
+//     texto = '\nIdade não é aceitável, insira uma idade em anos'
+//   }
+//   else if(idade == 1){
+//     texto =  'y mi edad es ' + idade + ' ano'
+//   }
+//   else{
+//     texto = 'y mi edad es ' + idade + texto
+//   }
+    
+  const texto = (idade <= 1) ? 'ano' : 'anos'
+
+  return <Text>Hola, yo soy {getNomeCompleto(nome, sobrenome).toUpperCase()} {texto}</Text>
 }
 
 export default IFAL;
@@ -14,9 +27,9 @@ export default IFAL;
 function IFAL() {
   return (
     <View style={styles.container}>
-      <Cat nome = 'black' sobrenome='pink' idade={15}/>
-      <Cat nome = 'apolo' sobrenome='titã' idade={12}/>
-      <Cat nome = 'garfield'sobrenome='lasanha' idade={21}/>
+      <Cat nome = 'black' sobrenome='pink' idade={1}/>
+      <Cat nome = 'apolo' sobrenome='titã' idade={0}/>
+      <Cat nome = 'garfield'sobrenome='lasanha' idade={-2}/>
       <StatusBar style="auto" />
     </View>
   );
